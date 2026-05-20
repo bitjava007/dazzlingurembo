aa0888c3afadf35db77e3075a0fb005a3302ca59
bitjava007 <fistoncokola@gmail.com>
2026-05-20 03:08:48 +0000

    Fix formatting issue in api.ts

--- apps/web/src/lib/api.ts
+++ apps/web/src/lib/api.ts
@@ -7,7 +7,7 @@ export const api = axios.create({
   headers: {
     'Content-Type': 'application/json',
   },
-});
+}); 
 
 api.interceptors.request.use((config) => {
   if (typeof window !== 'undefined') {
